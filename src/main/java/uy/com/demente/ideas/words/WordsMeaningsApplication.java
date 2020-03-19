ab9bd4fbbe376263e94d96066626e6cd9f9e821e
Diegog09 <1987diegog@gmail.com>
2020-03-19 08:30:32 +0100

    Primer commit - Project words-meanings

---
+++ src/main/java/uy/com/demente/ideas/words/WordsMeaningsApplication.java
@@ -0,0 +1,11 @@
+package uy.com.demente.ideas.words;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordsMeaningsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WordsMeaningsApplication.class, args);
+	}
+}
---
+++ src/test/java/uy/com/demente/ideas/words/WordsMeaningsApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.com.demente.ideas.words;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordsMeaningsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
