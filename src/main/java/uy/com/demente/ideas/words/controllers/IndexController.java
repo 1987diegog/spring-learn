14aabd2a30a55c0ea4cd458d84d0b4c9de1caa5e
Diegog09 <1987diegog@gmail.com>
2020-03-19 14:37:02 +0100

    Se agrega RequestParam

--- src/main/java/uy/com/demente/ideas/words/controllers/IndexController.java
+++ src/main/java/uy/com/demente/ideas/words/controllers/IndexController.java
@@ -38,35 +38,4 @@ public class IndexController {
         // una plantilla que se llama (/resources/templates/index.html)
         return "index";
     }
-
-    @GetMapping("/profile")
-    public String profile(Model model) {
-
-        User user = new User();
-        user.setName("Diego");
-        user.setLastname("Gonzalez");
-        user.setEmail("1987diegog@gmail.com");
-
-        model.addAttribute("title", "Profile: " + user.getName());
-        model.addAttribute("user", user);
-
-        return "profile";
-    }
-
-    @GetMapping("/users")
-    public String userList(Model model) {
-
-        List<User> userList = Arrays.asList(new User("Diego", "Gonzalez", "1987diegog@gmail.com"),
-                new User("Irina", "Gonzalez", "iri@gmail.com"),
-                new User("Silvia", "Narbaiz", "silnarbaiz@gmail.com"),
-                new User("Ragnar", "Lothbrok", "ragnar@gmail.com"),
-                new User("Roger", "Federer", "rfederer@gmail.com"),
-                new User("Diego", "Forlan", "forlan@gmail.com"));
-
-        model.addAttribute("title", "User list");
-        model.addAttribute("users", userList);
-        return "user-list";
-    }
-
-
 }
---
+++ src/main/java/uy/com/demente/ideas/words/controllers/UsersController.java
@@ -0,0 +1,71 @@
+package uy.com.demente.ideas.words.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import uy.com.demente.ideas.words.models.User;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@RequestMapping("/app")
+public class UsersController {
+
+    // Lo utilizamos cuando queremos pasar datos que son comunes a dos o mas metodos
+    // handlers del controlador, (por ejemplo si tuvieraos un select de paises). En este
+    // caso @ModelAttribute carga title al controlador y podra ser accedido desde cualquier
+    // vista (Thymeleaf, HTML, etc)
+    @ModelAttribute("title")
+    public String title() {
+        return "Words Meanings [DementeIdeas]";
+    }
+
+    @GetMapping("/profile")
+    public String profile(Model model) {
+
+        User user = new User();
+        user.setName("Diego");
+        user.setLastname("Gonzalez");
+        user.setEmail("1987diegog@gmail.com");
+
+        model.addAttribute("title", "Profile: " + user.getName());
+        model.addAttribute("user", user);
+
+        return "profile";
+    }
+
+    @GetMapping("/users")
+    public String userList(Model model) {
+
+        List<User> userList = Arrays.asList(new User("Diego", "Gonzalez", "1987diegog@gmail.com"),
+                new User("Irina", "Gonzalez", "iri@gmail.com"),
+                new User("Silvia", "Narbaiz", "silnarbaiz@gmail.com"),
+                new User("Ragnar", "Lothbrok", "ragnar@gmail.com"),
+                new User("Roger", "Federer", "rfederer@gmail.com"),
+                new User("Diego", "Forlan", "forlan@gmail.com"));
+
+        model.addAttribute("title", "User list");
+        model.addAttribute("users", userList);
+        return "user-list";
+    }
+
+    @GetMapping("/view")
+    public String view(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("data", "View name: " + name);
+        return "view";
+    }
+
+    @GetMapping("/add-user")
+    public String view() {
+        return "add-user";
+    }
+
+
+
+
+
+}
